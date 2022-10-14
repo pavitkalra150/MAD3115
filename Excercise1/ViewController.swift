701c96f83b55c3e7a6fc4b8fa7d035dfcb6f53cf
pavitkalra150 <c0868150@mylambton.ca>
2022-10-14 18:57:36 -0400

    Exercise 1

--- Excercise1/ViewController.swift
+++ Excercise1/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var background: UIView!
+    
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var helloworldlbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
     }
 
-
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        helloworldlbl.backgroundColor = sender.backgroundColor
+        helloworldlbl.textColor = .white
+        helloworldlbl.text = "Updated text"
+        view.backgroundColor = .gray
+        
+    }
+    
 }
 
