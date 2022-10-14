b325f17c92e3107dc95ec8e2a1ad087c4e31f3d4
pavitkalra150 <c0868150@mylambton.ca>
2022-10-14 18:19:57 -0400

    Initial Commit

---
+++ Excercise1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Excercise1
+//
+//  Created by PAVIT KALRA on 2022-10-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
